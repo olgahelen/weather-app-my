252ed534896f9b1c5726cb9a36b77a5de80d44ae
olgahelen <109621843+olgahelen@users.noreply.github.com>
2022-07-31 14:34:28 +0300

    add function getForecast

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ dateElement.innerHTML = formatDate(currentTime);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
+
 function getForecast(coordinates) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
