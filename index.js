a07d34f868026bf1fbaa81e54bb9ff639b601812
olgahelen <109621843+olgahelen@users.noreply.github.com>
2022-07-31 14:21:43 +0300

    add  link api

--- index.js
+++ index.js
@@ -37,3 +37,9 @@ dateElement.innerHTML = formatDate(currentTime);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
